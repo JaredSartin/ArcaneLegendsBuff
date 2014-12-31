e57074fd5b71ff1b212380148d0e5ca484fd71eb
Jared Sartin <jared.sartin@sonydadc.com>
2014-12-31 10:36:40 -0800

    makes resize button work

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,12 +1,38 @@
+var isFullscreen = false;
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+    var bodyEl = document.querySelectorAll("body")[0];
+
+    bodyEl.classList.add("debuffed-game");
+
+    var resizeButton = document.createElement("div");
+    resizeButton.id = "resize-button";
+    resizeButton.onclick = function() {
+      isFullscreen = !isFullscreen;
+      bodyEl.classList.toggle("debuffed-game");
+      bodyEl.classList.toggle("buffed-game");
+      if(isFullscreen) {
+        resizeButtonImg.src = chrome.extension.getURL("src/images/fullscreen-exit.png");
+      } else {
+        resizeButtonImg.src = chrome.extension.getURL("src/images/fullscreen.png");
+      }
+    };
+
+    // var resizeButtonImg = document.createElement("img");
+    var resizeButtonImg = new Image();
+    resizeButtonImg.src = chrome.extension.getURL("src/images/fullscreen.png");
+
+    resizeButton.appendChild(resizeButtonImg);
+    bodyEl.appendChild(resizeButton);
+
+    // document.querySelectorAll("body")[0].classList.remove("debuffed-game");
+    //
+    // document.querySelectorAll("body")[0].classList.add("buffed-game");
+    // document.querySelectorAll("body")[0].classList.remove("buffed-game");
 
 	}
 	}, 10);
