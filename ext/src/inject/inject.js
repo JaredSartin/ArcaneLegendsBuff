9d1372b1454baeb3742085920c6a135129ffcc83
Jared Sartin <jared.sartin@sonydadc.com>
2015-01-05 09:18:00 -0800

    starts approach to button hijacking

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,4 +1,5 @@
 var isFullscreen = false;
+var popover = undefined;
 
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
@@ -30,6 +31,39 @@ chrome.extension.sendMessage({}, function(response) {
 
     resizeButton.appendChild(resizeButtonImg);
     bodyEl.appendChild(resizeButton);
+
+    listener.addEventListener("DOMSubtreeModified", hijackButtons)
 	}
 	}, 10);
 });
+
+function hijackButtons() {
+  var urls = document.querySelectorAll("div.url a");
+  for(i in urls) {
+    console.log(urls[i]);
+    if(urls[i].getAttribute) {
+      var href = urls[i].getAttribute("href");
+      var onclick = urls[i].getAttribute("onclick");
+      // var rel = urls[i].getAttribute("rel");
+
+      if(!!href && !onclick) {
+        urls[i].setAttribute("onclick", "showPopover('" + href + "'); return false;");
+      } else if(!href && !!onclick) {
+        // HIJACK!
+      }
+    }
+  };
+}
+
+function showPopover(url) {
+  console.log("TADA!");
+  // http://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script
+  // Create iFrame
+  // Resize iFrame
+  // Append iFrame
+};
+
+function clearPopover() {
+  // Remove iFrame
+  // Clear iFrame storage
+};
